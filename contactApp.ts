3d128f105dea807884660bb20eaaf212d127b801
Muhammad Nafay Khan <1602010@namal.edu.pk>
2023-12-27 21:48:31 +0500

    isolated each functionality, like db connection etc

--- contactApp.ts
+++ contactApp.ts
@@ -2,74 +2,67 @@ import { Client } from "pg";
 import { connectDB, closeDB } from "./dbConnection";
 import { PhoneBook } from "./customInterfacesAndTypes";
 class Model {
-  private dbClient: Client;
+  private dbClient!: Client | null;
 
   constructor() {
-    // Call an async function in the constructor
-    (async () => {
-      try {
-        await this.connect();
-        // Continue with other constructor logic if needed
-      } catch (err) {
-        console.error("Error in constructor:", err);
-      }
-    })();
+    this.dbClient = null;
   }
 
-  private async connect(): Promise<void> {
+  async connect(): Promise<void> {
     try {
       this.dbClient = await connectDB();
       console.log("Connected to the database");
-    } catch (error) {
-      console.error("Error connecting to the database:", error);
+    } catch (err) {
+      console.log("Failure in database connection!\n", err);
     }
   }
 
-  private async disconnect(): Promise<void> {
+  async disconnect(): Promise<void> {
     try {
-      await closeDB(this.dbClient);
-      console.log("Disconnected from the database");
+      if (this.dbClient) {
+        await closeDB(this.dbClient);
+        console.log("Disconnected from the database");
+      }
     } catch (error) {
       console.error("Error closing the database connection:", error);
     }
   }
-  async insertOne(contact: PhoneBook): Promise<boolean> {
-    if (!this.dbClient) {
-      console.error("Database connection not established");
-      return false;
-    }
-    const { name, phoneNo, email, address, password } = contact;
-
+  async insertOne(contact: PhoneBook): Promise<void> {
     try {
-      const query = `INSERT INTO contacts (name, phoneNo, email, address, password) VALUES ($1, $2, $3, $4, $5) RETURNING id`;
+      if (this.dbClient) {
+        const { name, phoneNo, email, address, password } = contact;
 
-      const values = [name, phoneNo, email, address, password];
+        const query = `INSERT INTO contacts (name, phoneNo, email, address, password) VALUES ($1, $2, $3, $4, $5) RETURNING id`;
 
-      const result = await this.dbClient.query(query, values);
+        const values = [name, phoneNo, email, address, password];
 
-      console.log(`Contact inserted with ID: ${result.rows[0].id}`);
-      return true;
+        const result = await this.dbClient.query(query, values);
+
+        console.log(`Contact inserted with ID: ${result.rows[0].id}`);
+      }
     } catch (error) {
       console.error("Error inserting contact:", error);
-      return false;
     }
   }
 }
-// Example usage
-const myModel = new Model();
 
-// Usage example
-myModel
-  .insertOne({
-    name: "John Doe",
-    phoneNo: "1234567890",
-    email: "john.doe@example.com",
-    address: "123 Main St",
-    password: "securePassword",
-  })
-  .then((success) => {
-    console.log("Insert success:", success);
-  })
-  .catch((error) => {
+(async () => {
+  const myModel = new Model();
+
+  try {
+    await myModel.connect();
+
+    await myModel.insertOne({
+      name: "John Doe",
+      phoneNo: "1234567890",
+      email: "john.doe@example.com",
+      address: "123 Main St",
+      password: "securePassword",
+    });
+  } catch (error) {
     console.error("Error:", error);
-  });
+  } finally {
+    // Ensure to disconnect from the database after the operation
+    await myModel.disconnect();
+  }
+})();
